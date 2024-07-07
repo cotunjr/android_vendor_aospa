d72a0011b28374dd8e01686575f702ab166e393a
cotunjr <75676600+cotunjr@users.noreply.github.com>
2024-07-07 14:19:10 +0300

    Create aospa_mayfly.mk

---
+++ products/mayfly/aospa_mayfly.mk
@@ -0,0 +1,33 @@
+#
+# Copyright (C) 2024 Paranoid Android
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+ifeq (aospa_xiaomi12s,$(TARGET_PRODUCT))
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit_only.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
+
+# Inherit from the device configuration.
+$(call inherit-product, device/xiaomi/mayfly/device.mk)
+
+# Inherit from the AOSPA configuration.
+$(call inherit-product, vendor/aospa/target/product/aospa-target.mk)
+
+PRODUCT_BRAND := Xiaomi
+PRODUCT_DEVICE := mayfly
+PRODUCT_MANUFACTURER := Xiaomi
+PRODUCT_MODEL := 2206123SC
+PRODUCT_NAME := aospa_mayfly
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    TARGET_PRODUCT=mayfly
+
+PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
+
+# Boot Animation
+TARGET_BOOT_ANIMATION_RES := 1080
+
+endif
